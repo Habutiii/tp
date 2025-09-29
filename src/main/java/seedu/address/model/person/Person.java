4254afdfe4315ef01d0ae4cc8ff81e402524ea06
robinlogic <robstudios638@gmail.com>
2025-09-29 14:21:27 +0800

    Revert "add tutorial 2 stuff"
    This reverts commit 6a731383c78318c88498e27ee416dbe14ec217ff.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -20,7 +20,6 @@ public class Person {
     private final Name name;
     private final Phone phone;
     private final Email email;
-    private final Remark remark;
 
     // Data fields
     private final Address address;
@@ -29,13 +28,12 @@ public class Person {
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Remark remark, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
-        this.remark = remark;
         this.tags.addAll(tags);
     }
 
@@ -53,10 +51,6 @@ public class Person {
 
     public Address getAddress() {
         return address;
-    }
-
-    public Remark getRemark() {
-        return remark;
     }
 
     /**
