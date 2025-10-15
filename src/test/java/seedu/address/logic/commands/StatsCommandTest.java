9e24bb992d7649ceac4d1e7b7ca2d94030b3f688
robinlogic <robstudios638@gmail.com>
2025-10-15 22:09:55 +0800

    Add ObservablePersonList stub in StatsCommandTest

--- src/test/java/seedu/address/logic/commands/StatsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/StatsCommandTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Optional;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.index.Index;
@@ -104,7 +110,7 @@ public class StatsCommandTest {
 
         @Override
         public ObservableList<Person> getFilteredPersonList() {
-            return null;
+            return new PersonList();
         }
 
         @Override
@@ -137,6 +143,177 @@ public class StatsCommandTest {
         String manual = cmd.man();
         assertTrue(manual.contains("stats"));
         assertTrue(manual.contains("SEE MORE"));
+    }
+
+    /**
+     * Stub for ObservableList<Person> Object in ModelStub for unit testing.
+     */
+    private class PersonList implements ObservableList<Person> {
+
+        @Override
+        public void addListener(ListChangeListener<? super Person> listener) {
+
+        }
+
+        @Override
+        public void removeListener(ListChangeListener<? super Person> listener) {
+
+        }
+
+        @Override
+        public boolean addAll(Person... elements) {
+            return false;
+        }
+
+        @Override
+        public boolean setAll(Person... elements) {
+            return false;
+        }
+
+        @Override
+        public boolean setAll(Collection<? extends Person> col) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Person... elements) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Person... elements) {
+            return false;
+        }
+
+        @Override
+        public void remove(int from, int to) {
+
+        }
+
+        @Override
+        public int size() {
+            return 3;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @Override
+        public Iterator<Person> iterator() {
+            return null;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @Override
+        public <T> T[] toArray(T[] a) {
+            return null;
+        }
+
+        @Override
+        public boolean add(Person person) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends Person> c) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int index, Collection<? extends Person> c) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> c) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Person get(int index) {
+            return null;
+        }
+
+        @Override
+        public Person set(int index, Person element) {
+            return null;
+        }
+
+        @Override
+        public void add(int index, Person element) {
+
+        }
+
+        @Override
+        public Person remove(int index) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<Person> listIterator() {
+            return null;
+        }
+
+        @Override
+        public ListIterator<Person> listIterator(int index) {
+            return null;
+        }
+
+        @Override
+        public List<Person> subList(int fromIndex, int toIndex) {
+            return List.of();
+        }
+
+        @Override
+        public void addListener(InvalidationListener listener) {
+
+        }
+
+        @Override
+        public void removeListener(InvalidationListener listener) {
+
+        }
     }
 }
 
