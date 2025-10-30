6c7e9e184e8a847d38e65108f605a8c04e438de1
robinlogic <robstudios638@gmail.com>
2025-10-30 23:28:09 +0800

    Fix test case

--- src/test/java/seedu/address/ui/EditPreviewBuilderTest.java
+++ src/test/java/seedu/address/ui/EditPreviewBuilderTest.java
@@ -383,7 +383,7 @@ public class EditPreviewBuilderTest {
 
         FieldPreview tagPreview = previews.get(previews.size() - 1);
         assertEquals("Tags (t/):", tagPreview.getLabel());
-        assertEquals(EditCommand.MESSAGE_EXCEEDING_MAX_TAGS, tagPreview.getValue());
+        assertEquals(String.format(EditCommand.MESSAGE_EXCEEDING_MAX_TAGS, Person.MAX_TAGS_PER_PERSON, tags.size() + 1), tagPreview.getValue());
         assertFalse(tagPreview.isValid());
     }
 
